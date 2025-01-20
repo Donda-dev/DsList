46943cade90597e616fe0e26cdff9cf399d8f51d
Donda-dev <donda897@gmail.com>
2025-01-20 17:58:33 -0300

    project created

---
+++ src/test/java/com/dondadev/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dondadev.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
